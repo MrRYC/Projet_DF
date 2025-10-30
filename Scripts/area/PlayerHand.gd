01b0d7821a8bf62b984f267a1aa39c54cdb23dff
Raymond Coffinet <raymond.coffinet@gmail.com>
2025-10-30 17:42:07 +0100

    refonte de la gestion de la main v01.02

--- Scripts/area/PlayerHand.gd
+++ Scripts/area/PlayerHand.gd
@@ -9,7 +9,8 @@ const HAND_Y_POSITION = 950 #hauteur de la zone des cartes en main
 
 #variables du script
 @onready var center_screen_x = get_viewport().size.x / 2
-var card_position_in_hand = []
+var cards_in_hand = []
+var cards_position_in_hand = []
 var speed = Global.DEFAULT_CARD_MOVE_SPEED
 var new_turn : bool = false
 var hand_size : int
@@ -26,6 +27,7 @@ func _ready() -> void:
 func add_card_to_hand(card, index):
 	if !card.in_hand:
 		card.in_hand = true
+		cards_in_hand.insert(0,card)
 		update_hand_positions(card, index)
 	elif card.in_hand:
 		animate_card_to_position(card, card.starting_position)
@@ -33,6 +35,7 @@ func add_card_to_hand(card, index):
 func remove_card_from_hand(card):
 	if card.in_hand:
 		card.in_hand = false
+		cards_in_hand.erase(card)
 		update_hand_positions(card, 1)
 
 ###########################################################################
@@ -40,9 +43,9 @@ func remove_card_from_hand(card):
 ###########################################################################
 
 func calculate_hand_size():
-	if card_position_in_hand.size() > 0:
-		hand_x_position_min = float(card_position_in_hand.min())
-		hand_x_position_max = float(card_position_in_hand.max())
+	if cards_position_in_hand.size() > 0:
+		hand_x_position_min = float(cards_position_in_hand.min())
+		hand_x_position_max = float(cards_position_in_hand.max())
 	else:
 		hand_x_position_min = 0.0
 		hand_x_position_max = 0.0
@@ -57,7 +60,7 @@ func update_hand_positions(card, index):
 
 	#Position de la nouvelle carte en fonction de l'index
 	var x_position = calculate_card_position(index)
-	card_position_in_hand.append(x_position)
+	cards_position_in_hand.append(x_position)
 	var new_position = Vector2(x_position, HAND_Y_POSITION)
 	card.starting_position = new_position
 	animate_card_to_position(card, new_position)
@@ -66,35 +69,45 @@ func update_hand_positions(card, index):
 func move_card_to_index(card, mouse_pos_x):
 	var player_hand_min_zone = Vector2(hand_x_position_min, 775)
 	var player_hand_max_zone = Vector2(hand_x_position_max, HAND_Y_POSITION)
-	var card_index = get_card_index(card)
-	print("carte tiré position : "+str(card_index))
 	var new_index = get_drop_index(mouse_pos_x)
 	
-	for i in card_position_in_hand:
+	for i in cards_position_in_hand:
+		print(cards_position_in_hand)
+		var card_orignal_position = card.starting_position
+		var card_index = get_card_index(card)
 		if is_in_bounds(card.position, player_hand_min_zone, player_hand_max_zone):
-			if new_index == 0 :
-				var new_position = Vector2(hand_x_position_min, HAND_Y_POSITION)
-				card.starting_position = new_position
-				animate_card_to_position(card, new_position)
+			if new_index == 0 && card_index == 0 :
+				animate_card_to_position(card, card_orignal_position)
 			else:
-				while card_index <= new_index:
-					
-					update_hand_positions(card, i)
-
+				print("la carte tirée en position : "+str(card_index)+" remplace la carte en position : "+str(new_index))
+				var rebind_position : Array = cards_position_in_hand.duplicate()
+				var tmp : int = card_index
+				if new_index > card_index:
+					while card_index <= new_index:
+						print(cards_position_in_hand[card_index])
+						print(rebind_position[tmp].starting_position)
+						cards_position_in_hand[card_index].starting_position = rebind_position[tmp].starting_position
+						card_index += 1
+						tmp -= 1
+				else:
+					print("remplace une carte plus à gauche")
+					while card_index <= new_index:
+						pass
+				animate_card_to_position(card, card.starting_position)
 		elif !is_in_bounds(card.position, player_hand_min_zone, player_hand_max_zone):
-			print(card.starting_position)
 			animate_card_to_position(card, card.starting_position)
 
 func get_card_index(card):
-	print(card.starting_position)
-	for i in card_position_in_hand.size():
-		if card_position_in_hand[i] > card.starting_position:
+	for i in cards_position_in_hand.size():
+		if cards_position_in_hand[i] > card.starting_position.x:
 			return i
+	return cards_position_in_hand.size()
 
 func get_drop_index(mouse_pos_x):
-	for i in card_position_in_hand.size():
-		if card_position_in_hand[i] > mouse_pos_x:
+	for i in cards_position_in_hand.size():
+		if cards_position_in_hand[i] > mouse_pos_x:
 			return i
+	return cards_position_in_hand.size()
 
 func calculate_card_position(index):
 	var total_width = (hand_size - 1) * CARD_WIDTH
